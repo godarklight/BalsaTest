fa30602e915ae9bcb381a2ef89717960405c6a41
Christopher Andrews <godarklight@gmail.com>
2020-08-22 22:40:16 +1000

    Initial commit

---
+++ BalsaTest/Loader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BalsaCore;
+namespace BalsaTest
+{
+    //Without this file Balsa craps out on the load
+    [BalsaAddon]
+    public class Loader
+    {
+        [BalsaAddonInit]
+        public static void BalsaInit()
+        {
+
+        }
+
+        [BalsaAddonFinalize]
+        public static void BalsaFinalize()
+        {
+
+        }
+    }
+}
---
+++ BalsaTest/TestModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Modules;
+using UnityEngine;
+
+namespace BalsaTest
+{
+    public class TestModule : PartModule
+    {
+        public override void OnModuleSpawn()
+        {
+            Debug.Log("[TestModule] Spawned");
+        }
+    }
+}
